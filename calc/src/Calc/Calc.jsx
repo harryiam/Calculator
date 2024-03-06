ddf43f5ae58597d1267af38fcdef76dac3b0e665
AMBATI HARITHA <ambatiharitha@AMBATIs-MacBook-Air.local>
2024-03-06 23:10:47 +0530

    complete calc

--- calc/src/Calc/Calc.jsx
+++ calc/src/Calc/Calc.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from "react";
+import styles from "./Calc.module.css";
 
 function Calc() {
   const [display, setDisplay] = useState("");
@@ -21,34 +22,34 @@ function Calc() {
   };
 
   return (
-    <div>
-      <h3>React Calculator</h3>
-      <input type="text" value={display} readOnly />
-      <div>{result}</div>
-      <div>
+    <div className={styles.calculator}>
+        <h3>React Calculator</h3>
+      <input type="text" value={result?(result):(display)} readOnly className={styles.display} />
+      <div className={styles.buttons}>
         <button onClick={() => handleButtonClick("7")}>7</button>
         <button onClick={() => handleButtonClick("8")}>8</button>
         <button onClick={() => handleButtonClick("9")}>9</button>
         <button onClick={() => handleButtonClick("+")}>+</button>
       </div>
-      <div>
+      <div className={styles.buttons}>
         <button onClick={() => handleButtonClick("4")}>4</button>
         <button onClick={() => handleButtonClick("5")}>5</button>
         <button onClick={() => handleButtonClick("6")}>6</button>
         <button onClick={() => handleButtonClick("-")}>-</button>
       </div>
-      <div>
+      <div className={styles.buttons}>
         <button onClick={() => handleButtonClick("1")}>1</button>
         <button onClick={() => handleButtonClick("2")}>2</button>
         <button onClick={() => handleButtonClick("3")}>3</button>
         <button onClick={() => handleButtonClick("*")}>*</button>
       </div>
-      <div>
+      <div className={styles.buttons}>
         <button onClick={() => handleButtonClick("C")}>C</button>
         <button onClick={() => handleButtonClick("0")}>0</button>
         <button onClick={() => handleButtonClick("=")}>=</button>
         <button onClick={() => handleButtonClick("/")}>/</button>
       </div>
+      {/* <div className={styles.result}>{result}</div> */}
     </div>
   );
 }
